9ced1a8e58f594ef12f5564ace5a25bbcc67f30f
unknown <luciano.sarcinella@gmail.com>
2021-12-02 12:56:40 -0300

    server con express + getRandom

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
+let fs = require ("fs")
 let express = require ("express");
 let app = express();
 const PORT = 8080;
 let archivo = "./archivos.txt"
+let path = require("path")
 let Handle = require("./manejarArchivos");
 
 let manejarArchivos = new Handle(archivo)
 
 app.get("/", (req,res,next)=>{
-    res.send("<p style='color:blue;'>Mi primer servidor con express</p>")
+    res.send("<p style='color:blue;'>Server de luchi con express</p>")
 })
 app.get("/productos", async (req,res,next)=>{
-    const info = await manejarArchivos.getProductos()
-    res.send(info);
+    devolverProductos(req,res);
+}); 
+app.get("/productosRandom", (req,res,next)=>{
+    devolverProductosRandom(req,res);
 });
+
+
 app.listen(PORT, ()=>{
     console.log(`Mi servidor escuchando desde http://localhost:${PORT}`)
 })
+
+function devolverProductos(req, res){
+    manejarArchivos.getAll().then(data => {
+        res.send(data);
+    }).catch(error => {
+        res.send(error);
+    });
+
+} 
+function devolverProductosRandom(req, res){
+    manejarArchivos.getbyId(Math.floor(Math.random() * 4) + 1).then(data => {
+        res.send(data);
+    }).catch(error => {
+        res.send(error);
+    });
+
+}
--- manejarArchivos.js
+++ manejarArchivos.js
@@ -1,13 +1,70 @@
 let fs = require("fs");
+let path = require("path")
 
 class Handle {
-    constructor (archivo){
-        thiss.archivo = archivo;
+    constructor(url_){
+        this.url = url_;
     }
-    async getProductos(){
-        let producto = await fs.promises.readFile(`${this.archivo}`, 'utf-8')
-        return (JSON.parse(objeto))
+    async  getAll(){
+        try{
+            let data = await fs.promises.readFile(this.url,'utf-8');
+            return JSON.parse(data);
+        }catch(error){
+            return [];
+        }
+    }
+    async deleteById(id){
+        try{
+            let resp = [];
+            let data = await this.getAll();
+            for(const key in data){
+                if(data[key].id == id){
+                    data.splice(key, 1);
+                }
+                console.log(data);
+            }
+            let content = JSON.stringify(data, null,2);
+            await fs.promises.writeFile(this.url, content);
+            return data;
+        }catch(error){
+            console.log(error);
+        }
+    }
+    async getbyId(id){
+        try{
+            let resp = [];
+            let data = await this.getAll();
+            return data.find(producto => producto.id == id);
+            
+        }catch(error){
+            console.log(error);
+        }
     }
 }
 
 module.exports = Handle
+
+
+                    // async save(producto){
+                    //     try{
+                    //         let data = await this.getAll();
+                    //         let newId = await this.getNewId(data);
+                    //         let newProducto = {id: newId, ...producto};
+                    //         data.push(newProducto);
+                    //         let content = JSON.stringify(data, null,2);
+                    //         await fs.promises.writeFile(this.url, content);
+                    //         return newId;
+                    //     }catch(error){
+                    //         console.log(error);
+                    //     }
+                    // }
+                    // async getNewId(productos){
+                    //     try{
+                    //         let resp = productos.reduce((max, producto) => {
+                    //             return producto.id > max ? producto.id : max;
+                    //         }, 0);
+                    //         return resp + 1;
+                    //     }catch(error){
+                    //         console.log(error);
+                    //     }
+                    // }
